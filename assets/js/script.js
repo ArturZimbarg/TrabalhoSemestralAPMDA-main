dc5f6bce336c5051955a30dac379d236062781d3
ArturZimbarg <86981718+ArturZimbarg@users.noreply.github.com>
2023-04-13 21:19:09 -0300

    Menu hamburguer feito, agora é partir pro abraço

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 console.log("calcular orcamento")
 
 //Manipulando DOM
-let inputQtde = document.querySelection("#qtde")
-let inputJs = document.querySelector("js")
+let inputLayout_JS = document.querySelector("#layout-sim_JS")
+let inputLayoutNao_JS = document.querySelector("#layout-nao_JS")
 let inputLayout = document.querySelector("#layout-sim")
 let inputLayoutNao = document.querySelector("#layout-nao")
 let inputPrazo = document.querySelector("#prazo")
 
-inputQtde.addEventListner("change", atualizarOrcamento)
+
 inputJs.addEventListener("change", atualizarOrcamento)
+inputLayout_JS.addEventListener("change", atualizarOrcamento)
+inputLayoutNao_JS.addEventListener("change", atualizarOrcamento)
 inputLayout.addEventListener("change", atualizarOrcamento)
 inputLayoutNao.addEventListener("change", atualizarOrcamento)
 
